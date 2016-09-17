5f0e66424b32e9f2fa0071aee92b3b8074bacc0d
ZengTianShengZ <zengtianshengz@163.com>
2016-09-17 21:26:43 +0800

    使用 gulp 插件 对代码进行压缩

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var uglify = require('gulp-uglify'); // 压缩 js 的文件
+var filter = require('gulp-filter'); // 用于帅选文件，还有放会文件，就是读取流，读完后再把文件放回流中
+var rev = require('gulp-rev');
+var revReplace = require('gulp-rev-replace');
+var useref = require('gulp-useref');
+var csso = require('gulp-csso'); // 压缩 css 的文件
+var rename = require('gulp-rename');
+
+
+// 合并，压缩文件，将 js文件夹下的js文件合并压缩成 all.js 放在 /dist文件夹下
+gulp.task('scripts', function() {
+    gulp.src('./zBase-1.1.0/zBase-1.1.0.js')
+       // .pipe(concat('all.js')) // concat 合并
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('zBase-1.1.0.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
--- zBase-1.1.0/test.js
+++ zBase-1.1.0/test.js
@@ -2,27 +2,10 @@
  * Created by Administrator on 2016/9/17.
  */
 
-
-$('.d-c').css({
-    width:'300px',
-    height:'300px',
+$('.div-c').css({
+    width:'100px',
+    height:'100px',
     background:'red'
 })
 
 
-$('li').getElement(2).css({
-    width:'300px',
-    height:'300px',
-    background:'red'
-}).click(function () {
-    alert("vvvvvvv")
-});
-//alert( xxv() );
-
-
-$('.dd').centerInWindow(100,100);
-
-
-//var arr = [12,323,43,434,54,5];
-
-//alert(   $().inArray(arr,12)  );
