c712a5b870c1e6768b8790104e9e0e711b4c531d
曾思胜 <2292800085@qq.com>
2017-03-07 14:12:34 +0800

    添加 AMD 规范测试

---
+++ test2/app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bong on 2017/3/7.
+ */
+require.config({
+    baseUrl:"../lib",
+    paths:{
+        "zbase": "zBase-1.2.0"
+    }
+});
+
+require(["zbase"],function($){
+
+    console.log('.....');
+
+    $('.test').css({
+        width:'300px',
+        height:'300px',
+        color:"white",
+        'font-size':'40px',
+        background:"red"
+    });
+});
